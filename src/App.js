bce87d6bcb7acb512e0ed67f5132928ccbad01e0
theo <teddyhoover@gmail.com>
2021-05-07 11:46:57 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import PlayersContainer from "./containers/PlayersContainer";
 
 class App extends Component {
   render() {
     return(
-      <div className="App">
-        <PlayersContainer store={this.props.store}/>
-      </div>
+      <Router>
+        <div className="App">
+          <Route path='/' component={() => <PlayersContainer store={this.props.store}/>}/>
+        </div>
+      </Router>
     )
   };
 }
