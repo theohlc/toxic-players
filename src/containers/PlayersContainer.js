d13758a018618af326f618dc16f5b92eadf6e3c3
theo <teddyhoover@gmail.com>
2021-05-24 13:58:30 -0500

    added sort

--- src/containers/PlayersContainer.js
+++ src/containers/PlayersContainer.js
@@ -1,15 +1,34 @@
+import { Button } from "bootstrap";
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPlayers } from "../actions/players";
 import PlayerList from "../components/PlayerList";
 
 class PlayersContainer extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.playersList = this.props.players.sort((a, b) => (a.username > b.username) ? 1 : -1)
+
+        this.state = {
+            playersSorted: false
+        }
+    }
+
     render(){
         return(
-            <div className='container-fluid bcontent'>
-                <PlayerList players={this.props.players}/>
+            <div>
+                <button onClick={this.handleClick}>Sort</button>
+                <div className='container-fluid bcontent'>
+                    <PlayerList players={this.state.playersSorted ? this.playersList : this.props.players}/>
+                </div>
             </div>
+            
         )
+    }
+
+    handleClick = () => {
+        this.state.playersSorted ? this.setState({playersSorted: false}) : this.setState({playersSorted: true})
     }
 
     handleOnSubmit = (event) => {
